0e549b568586b68c2970cabd8a60bb99a1a577be
nuc130n <cedric.gain@econocom.com>
2025-11-14 03:59:21 +0100

    Update EndNote 20 installation script

--- fragments/labels/endnote20.sh
+++ fragments/labels/endnote20.sh
@@ -1,9 +1,9 @@
 endnote20)
     name="EndNote 20"
-    type="appInDmg"
+    type="dmg"
     downloadURL="https://download.endnote.com/downloads/20/EndNote20Installer.dmg"
-    appNewFile="Install EndNote 20.app"
-    installerTool="/Applications/Install EndNote 20.app/Contents/MacOS/Install EndNote"
+    appName="Install EndNote 20.app"
+    installerTool="$appPath/Contents/MacOS/Install EndNote"
     installerArgs=( "-silent" )
     expectedTeamID="JQ525L2MZD"
     ;;
