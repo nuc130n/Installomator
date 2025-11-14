ff15d6aaee9282767fe5ca8d4df2d3be7825d279
nuc130n <cedric.gain@econocom.com>
2025-11-14 03:17:37 +0100

    Add EndNote 21 installation script

---
+++ fragments/labels/endnote21.sh
@@ -0,0 +1,7 @@
+endnote21)
+    name="EndNote 21"
+    type="dmg"
+    downloadURL="https://download.endnote.com/downloads/21/EndNote21Installer.dmg"
+    appName="EndNote 21.app"
+    expectedTeamID="JQ525L2MZD"
+    ;;
