49c738d58eae111b4744c3c12a83a4a8fb2b0851
nuc130n <cedric.gain@econocom.com>
2025-11-14 03:16:37 +0100

    Add EndNote 20 installation script

---
+++ fragments/labels/endnote.sh
@@ -0,0 +1,7 @@
+endnote20)
+    name="EndNote 20"
+    type="dmg"
+    downloadURL="https://download.endnote.com/downloads/20/EndNote20Installer.dmg"
+    appName="EndNote 20.app"
+    expectedTeamID="JQ525L2MZD"
+    ;;
