ecb2fccfaf7433fef08658e1edd27019d6c90ea1
nuc130n <cedric.gain@econocom.com>
2025-11-14 01:10:47 +0100

    Fix download URL extraction for Adobe Digital Editions

--- fragments/labels/adobedigitaleditions.sh
+++ fragments/labels/adobedigitaleditions.sh
@@ -1,7 +1,7 @@
 adobedigitaleditions)
     name="Adobe Digital Editions"
     type="pkgInDmg"
-    downloadURL=$(curl -fs https://www.adobe.com/solutions/ebook/digital-editions/download.html | grep dmg | sed -n 's/.*href="\([^"]*\)".*/\1/p')
+    downloadURL=$(curl -fs https://www.adobe.com/solutions/ebook/digital-editions/download.html | grep -o 'https://adedownload\.adobe\.com/pub/adobe/digitaleditions/.*\.dmg' | head -n 1 | sed -n 's/.*href="\([^"]*\)#.*".*/\1/p')
     appNewVersion=$(curl -fs https://www.adobe.com/solutions/ebook/digital-editions/download.html | grep -o 'Adobe Digital Editions.*Installers' | awk -F' ' '{ print $4 }')
     expectedTeamID="JQ525L2MZD"
     ;;
