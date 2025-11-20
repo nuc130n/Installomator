ad1165953cbc032d627a10d1c160ef4201740804
nuc130n <cedric.gain@econocom.com>
2025-11-20 17:09:33 +0100

    Refactor appNewVersion command for clarity

--- fragments/labels/krita.sh
+++ fragments/labels/krita.sh
@@ -3,11 +3,7 @@ krita)
     name="krita"
     type="dmg"
     # Detect latest version from KDE stable directory listing
-    appNewVersion=$(curl -fs "https://download.kde.org/stable/krita/" \
-        | grep -Eo 'href="[0-9]+\.[0-9]+\.[0-9]+/' \
-        | sed -E 's/href="([0-9.]+)\/"/\1/' \
-        | sort -V \
-        | tail -1)
+    appNewVersion=$(curl -fs "https://download.kde.org/stable/krita/" | grep -Eo 'href="[0-9]+\.[0-9]+\.[0-9]+/' | sed -E 's/href="([0-9.]+)\/"/\1/' | sort -V | tail -1)
     # Build URL to the signed DMG
     downloadURL="https://download.kde.org/stable/krita/${appNewVersion}/krita-${appNewVersion}_signed.dmg"
     expectedTeamID="5433B4KXM8"
