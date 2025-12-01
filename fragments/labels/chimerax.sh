f6ae95397cc97dd1962c8f03f29632278e6f26f8
nuc130n <cedric.gain@econocom.com>
2025-12-01 16:12:56 +0100

    Update chimerax.sh

--- fragments/labels/chimerax.sh
+++ fragments/labels/chimerax.sh
@@ -3,8 +3,8 @@ chimerax)
     type="dmg"
     appNewVersion="1.10.1"
     fileParam="${appNewVersion}/mac_universal/ChimeraX-${appNewVersion}.dmg"
-    downloadURL="https://www.cgl.ucsf.edu/chimerax/cgi-bin/secure/chimerax-get.py?file=${fileParam}"
-    curlOptions=( --data "accept=Accept" )
+    downloadURL="https://www.cgl.ucsf.edu/chimerax/cgi-bin/secure/chimerax-get.py"
+    curlOptions=( --data "file=${fileParam}" --data "choice=Accept" )
     appName="ChimeraX-${appNewVersion}.app"
     expectedTeamID="LWV8X224YF"
     blockingProcess=( "ChimeraX" )
