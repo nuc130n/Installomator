ef17e277dbc2e0062a9079ff0c10fad771e009db
nuc130n <cedric.gain@econocom.com>
2025-12-01 16:02:40 +0100

    Update chimerax.sh

--- fragments/labels/chimerax.sh
+++ fragments/labels/chimerax.sh
@@ -1,12 +1,11 @@
 chimerax)
     name="ChimeraX"
     type="dmg"
-    downloadPage="https://www.cgl.ucsf.edu/chimerax/download.html"
-    appNewVersion=$(curl -sL "$downloadPage" | grep -oE "ChimeraX-[0-9]+\.[0-9]+(\.[0-9]+)?\.dmg" | sed -E 's/ChimeraX-([0-9]+\.[0-9]+(\.[0-9]+)?)\.dmg/\1/' | sort -V | tail -1)
+    appNewVersion="1.10.1"
     fileParam="${appNewVersion}/mac_universal/ChimeraX-${appNewVersion}.dmg"
     downloadURL="https://www.cgl.ucsf.edu/chimerax/cgi-bin/secure/chimerax-get.py?file=${fileParam}"
     curlOptions=( --data "accept=Accept" )
     appName="ChimeraX-${appNewVersion}.app"
     expectedTeamID="LWV8X224YF"
-    blockingProcesses=( "ChimeraX" )
+    blockingProcess=( "ChimeraX" )
     ;;
