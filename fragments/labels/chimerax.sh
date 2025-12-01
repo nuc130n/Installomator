82aed8693e661abc586f4770552e36c33efa42d4
nuc130n <cedric.gain@econocom.com>
2025-12-01 16:23:56 +0100

    Update chimerax.sh

--- fragments/labels/chimerax.sh
+++ fragments/labels/chimerax.sh
@@ -1,10 +1,14 @@
 chimerax)
     name="ChimeraX"
     type="dmg"
-    appNewVersion="1.10.1"
-    fileParam="${appNewVersion}/mac_universal/ChimeraX-${appNewVersion}.dmg"
+    jsonURL="https://www.rbvi.ucsf.edu/chimerax/data/release-info/enabled.json"
+    dmgCandidates=$(curl -sfL "$jsonURL" | grep -oE 'ChimeraX-[0-9]+\.[0-9]+(\.[0-9]+)?\.dmg' | sort -uV)
+    latest=$(echo "$dmgCandidates" | sed -E 's/ChimeraX-([0-9]+\.[0-9]+(\.[0-9]+)?)\.dmg/\1/' | sort -V | tail -1)
+    if [ -z "$latest" ]; then appNewVersion="1.10.1"; else appNewVersion="$latest"; fi
+    dmgName="ChimeraX-${appNewVersion}.dmg"
+    fileParam="${appNewVersion}/mac_universal/${dmgName}"
     downloadURL="https://www.cgl.ucsf.edu/chimerax/cgi-bin/secure/chimerax-get.py"
-    curlOptions=( --data "file=${fileParam}" --data "choice=Accept" )
+    curlOptions=( --location --data "file=${fileParam}" --data "choice=Accept" )
     appName="ChimeraX-${appNewVersion}.app"
     expectedTeamID="LWV8X224YF"
     blockingProcess=( "ChimeraX" )
