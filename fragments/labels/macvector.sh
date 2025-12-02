fc8e0ba5d48bba0ae5d92ffb70211ffd5c1b8d09
nuc130n <cedric.gain@econocom.com>
2025-12-02 15:57:40 +0100

    Update macvector.sh

--- fragments/labels/macvector.sh
+++ fragments/labels/macvector.sh
@@ -4,5 +4,7 @@ macvector)
     downloadURL=$(curl -fsL "https://macvector.org/support/downloads/installers-and-updaters/" | grep -oE 'https://[^"]*MacVector[^"]*\.dmg' | head -1)
     appCustomDirectory="MacVector"
     appNewName="MacVector"
+    appPath="/Applications/MacVector/MacVector.app"
+    versionKey="CFBundleShortVersionString"
     expectedTeamID="BK85R8X44N"
     ;;
