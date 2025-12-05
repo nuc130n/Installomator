926b55b5a316f56b3eae3d9c20196bfa716e5558
nuc130n <cedric.gain@econocom.com>
2025-12-05 11:55:24 +0100

    Create forklift.sh

---
+++ fragments/labels/forklift.sh
@@ -0,0 +1,13 @@
+forklift)
+    name="ForkLift"
+    type="zip"
+    downloadURL="https://download.binarynights.com/ForkLift/ForkLift.zip"
+    appPathDetected="$(ls "$archivePath" | grep -i "forklift" | head -n 1)"
+    appPathDetected="$archivePath/$appPathDetected"
+    appNewVersion=$(defaults read "$appPathDetected/Contents/Info" CFBundleShortVersionString 2>/dev/null)
+    if [[ -z "$appNewVersion" ]]; then
+        appNewVersion=$(defaults read "$appPathDetected/Contents/Info" CFBundleVersion 2>/dev/null)
+    fi
+    expectedTeamID="YJ22L2T93R"
+    blockingProcesses=( "ForkLift" )
+    ;;
