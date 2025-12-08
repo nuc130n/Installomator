299dc84b892657696039e3e1e5f093c038c5c246
nuc130n <cedric.gain@econocom.com>
2025-12-08 10:42:55 +0100

    Update mzmine.sh

--- fragments/labels/mzmine.sh
+++ fragments/labels/mzmine.sh
@@ -2,8 +2,8 @@ mzmine)
     # credit: Cedric GAIN (@nuc130n)
     name="MZmine"
     type="dmg"
-    downloadURL="$(curl -sL https://github.com/mzmine/mzmine/releases/latest | grep -Eo 'https://github.com/mzmine/mzmine/releases/download/[^\"]*macOS_installer-[0-9.]*\.dmg' | head -n1)"
-    appNewVersion="$(basename "$downloadURL" | sed -E 's/.*macOS_installer-([0-9.]+)\.dmg/\1/')"
+	downloadURL="$(curl -sL https://github.com/mzmine/mzmine/releases/latest | grep -Eo 'https://github.com/mzmine/mzmine/releases/download/[^\" ]*mzmine_macOS_installer_academia-[0-9.]+\.dmg' | head -n1)"
+    appNewVersion="$(basename "$downloadURL" | sed -E 's/.*mzmine_macOS_installer_academia-([0-9.]+)\.dmg/\1/')"
     expectedTeamID="25Z887Z794"
     blockingProcesses=( mzmine )
     ;;
