c6504b64a056d6d8d58a7a66589bac140276ba46
nuc130n <cedric.gain@econocom.com>
2025-12-08 10:25:07 +0100

    Update Installomator.sh
    added mzmine.sh

--- Installomator.sh
+++ Installomator.sh
@@ -7609,6 +7609,15 @@ mysqlworkbenchce)
     appNewVersion="$(curl -fsL 'http://workbench.mysql.com/current-release' | grep fullversion | cut -d\" -f4).CE"
     expectedTeamID="VB5E2TV963"
     ;;
+mzmine)
+    # credit: Cedric GAIN (@nuc130n)
+    name="MZmine"
+    type="dmg"
+    downloadURL="$(curl -sL https://github.com/mzmine/mzmine/releases/latest | grep -Eo 'https://github.com/mzmine/mzmine/releases/download/[^\"]*macOS_installer-[0-9.]*\.dmg' | head -n1)"
+    appNewVersion="$(basename "$downloadURL" | sed -E 's/.*macOS_installer-([0-9.]+)\.dmg/\1/')"
+    expectedTeamID="25Z887Z794"
+    blockingProcesses=( mzmine )
+    ;;
 namiral)
     name="Namirial Sign"
     type="dmg"
