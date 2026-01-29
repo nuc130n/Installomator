6f3ed210a05c9a25b722e5ec17759e2ec9bc18dd
nuc130n <cedric.gain@econocom.com>
2026-01-30 00:24:04 +0100

    Add Zimbra Desktop support to Installomator.sh

--- Installomator.sh
+++ Installomator.sh
@@ -10896,6 +10896,13 @@ zerotier)
     downloadURL="https://download.zerotier.com/dist/ZeroTier%20One.pkg"
     expectedTeamID="8ZD9JUCZ4V"
     ;;
+zimbradesktop)
+    name="Zimbra Desktop"
+    type="dmg"
+    downloadURL=$(curl -fsL "https://www.zimbra.com/zimbra-desktop-download/" | grep -oE 'https://files\.zimbra\.com/downloads/[^"]+\.dmg' | head -1)
+    appNewVersion=$(echo "$downloadURL" | sed -E 's/.*Desktop_([0-9.]+)_.*/\1/')
+    expectedTeamID="ZT347VSDFT"
+    ;;
 zipwhip)
     name="Zipwhip"
     type="dmg"
